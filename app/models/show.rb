b37d7f5ae817fc55f7c50746298ba43baa8870f9
Nick Story <storynickolasl@gmail.com>
2022-10-02 09:24:47 -0400

    commit tree fdeced3af705942a642d5ed9416f462408fbc48a

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    selection = []
+    for actor in self.actors do
+      selection.push(actor.full_name)
+    end
+    selection
+  end
   
 end
